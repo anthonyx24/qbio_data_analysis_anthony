43ba28307f88c457b71f7b91bf4a87c5a0188dac
anthonyx24 <79112832+anthonyx24@users.noreply.github.com>
2021-03-03 14:40:08 -0800

    Update week4_first_data_analysis.R

--- R_code/week4_first_data_analysis.R
+++ R_code/week4_first_data_analysis.R
@@ -38,17 +38,21 @@ patient_ages <- patient_data$paper_age_at_initial_pathologic_diagnosis
 patient_data$age_category = ifelse(patient_ages < 40, "Young", ifelse(patient_ages >= 60, "Old", "Mid"))
 
 # Choose 3 genes of interest from the paper presentations 
-rowData(sum_exp)$ensembl_gene_id[rowData(sum_exp)$external_gene_name == "ESR1"] #returns the gene id for the name ESR1, boolean indexing
+# rowData(sum_exp)$ensembl_gene_id[rowData(sum_exp)$external_gene_name == "ESR1"] #returns the gene id for the name ESR1, boolean indexing
 
 # genes <- c("ENSG00000141510", "ENSG00000141736", "ENSG00000121879")
 patient_data$TP53_counts = htseq_counts["ENSG00000141510",] #indexing [row, column], leaving a blank will give all the rows/columns
 patient_data$ERBB2_counts = htseq_counts["ENSG00000141736",]
 patient_data$PIK3CA_counts = htseq_counts["ENSG00000121879",]
+patient_data$ESR1_counts = htseq_counts["ENSG00000091831",]
 
 # Create 3 different boxplots with age_category on x-axis, counts on the y-axis by repeating the below code for each gene
-boxplot(TP53_counts~age_category, data = "patient_data", main = "Boxplot of HTSeq - Counts for TP53 by Age Category")
-boxplot(ERBB2_counts~age_category, data = "patient_data", main = "Boxplot of HTSeq - Counts for ERBB2 by Age Category")
-boxplot(PIK3CA_counts~age_category, data = "patient_data", main = "Boxplot of HTSeq - Counts for PIK3CA by Age Category")
+png("boxplot_TP53.png")
+boxplot(TP53_counts~age_category, data = patient_data, main = "Boxplot of HTSeq - Counts for TP53 by Age Category")
+
+boxplot(ERBB2_counts~age_category, data = patient_data, main = "Boxplot of HTSeq - Counts for ERBB2 by Age Category")
+boxplot(PIK3CA_counts~age_category, data = patient_data, main = "Boxplot of HTSeq - Counts for PIK3CA by Age Category")
+boxplot(ESR1_counts~age_category, data = patient_data, main = "Boxplot of HTSeq - Counts for ESR1 by Age Category")
 
 # remember to give your plot a title and informative axis labels
 # png("boxplot_genename.png")
